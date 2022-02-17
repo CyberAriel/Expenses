276a64ec5fc5e36b4fff795318990a3ed0c4a639
CyberAriel <jastrzab.pp@gmail.com>
2022-02-17 17:53:44 +0100

    17.02.2022

---
+++ Expenses/Count.cs
@@ -0,0 +1,110 @@
+﻿public abstract class Count
+{
+    protected string name;
+    protected int cash;
+    protected string path = @"count.txt";
+    protected int i = 0 ;
+    protected int earnings = 0;
+    protected int expenses = 0;
+    protected int savings = 0;
+
+
+
+
+
+    public virtual void WriteText()
+    {
+        
+        StreamWriter sw;
+
+        if (!File.Exists(path))
+        {
+            sw = File.CreateText(path);
+        }
+        else
+        {
+            sw = new StreamWriter(path, true);
+        }
+       
+        sw.WriteLine(name +" "+ cash);
+        sw.Close();
+
+        
+    }
+    public virtual void ReadText()
+    {
+        StreamReader sr = File.OpenText(path);
+        string s = " ";
+        while ((s = sr.ReadLine()) != null)
+        {
+            Console.WriteLine(s);
+
+        }
+        sr.Close();
+    }
+    
+}
+public class Life : Count
+{
+
+    public Life(string name = "Empty", int cash = 0, string path="life.txt")
+    {
+        this.cash = cash;
+        this.name = name;
+        this.path = path;
+
+
+    }
+
+    public void Text()
+
+    {
+        WriteText();
+        ReadText();
+    }
+
+
+}
+public class Large : Count
+{
+ 
+}
+public class Pleasures : Count
+{
+    
+}
+public class Education : Count
+{
+
+}
+public class Freedom : Count
+{
+
+}
+public class Charity : Count
+{
+   
+}
+public class Earnings : Count
+{
+    public void Show()
+    {
+        Console.WriteLine("Your Ernings: " + earnings);
+    }
+}
+public class Expenses : Count
+{
+    public void Show()
+    {
+        Console.WriteLine("Your Expenses: " + expenses);
+    }
+}
+public class Savings : Count
+{
+    public void Show()
+    {
+        Console.WriteLine("Your Saving: " + savings);
+    }
+}
+
+
---
+++ Expenses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+
+Console.WriteLine("Welcome in Expenses");
+Console.WriteLine("Create by Patryk Pieczka");
+Earnings earnings = new Earnings();
+earnings.Show();
+Expenses expenses = new Expenses();
+expenses.Show();
+Savings savings = new Savings();
+savings.Show();
+Console.WriteLine("Select action");
+Console.WriteLine("1: Add a purchase");
+Console.WriteLine("2: Show a Purchase");
+Console.WriteLine("3: Determine your earnings");
+Console.WriteLine("4: Determine your expenses");
+Console.WriteLine("5: Determine your savings");
+
+
+
+
+
+
+Life life = new Life("meat" , 40);
+
+//life.WriteText();
+//life.ReadText();
+
+
+
+
+GC.Collect();
+//Console.ReadKey();
+
