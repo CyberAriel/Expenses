dc5a606742a04ece6dcc3c4231af59ebd30d38cf
CyberAriel <jastrzab.pp@gmail.com>
2022-02-18 10:20:39 +0100

    18.02.2022

--- Expenses/Count.cs
+++ Expenses/Count.cs
@@ -1,9 +1,10 @@
-﻿public abstract class Count
+﻿
+public abstract class Count
 {
     protected string name;
     protected int cash;
-    protected string path = @"count.txt";
-    protected int i = 0 ;
+    protected string path;
+    //protected int i = 0 ;
     protected int earnings = 0;
     protected int expenses = 0;
     protected int savings = 0;
@@ -14,12 +15,16 @@
 
     public virtual void WriteText()
     {
-        
+        Console.WriteLine("Enter the name of the product / service.");
+        name = Console.ReadLine();
+        Console.WriteLine("Enter the cost of the product / service.");
+        cash = Int32.Parse(Console.ReadLine());
         StreamWriter sw;
 
         if (!File.Exists(path))
         {
             sw = File.CreateText(path);
+            sw.WriteLine(path);
         }
         else
         {
@@ -33,15 +38,23 @@
     }
     public virtual void ReadText()
     {
-        StreamReader sr = File.OpenText(path);
-        string s = " ";
-        while ((s = sr.ReadLine()) != null)
+        if (!File.Exists(path))
         {
-            Console.WriteLine(s);
-
+            Console.WriteLine("File does not exist");
         }
-        sr.Close();
+        else
+        {
+            StreamReader sr = File.OpenText(path);
+            string s = " ";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+
+            }
+            sr.Close();
+        }
     }
+
     
 }
 public class Life : Count
@@ -56,34 +69,62 @@ public class Life : Count
 
     }
 
-    public void Text()
-
-    {
-        WriteText();
-        ReadText();
-    }
-
 
 }
 public class Large : Count
 {
- 
+    public Large(string name = "Empty", int cash = 0, string path = "large.txt")
+    {
+        this.cash = cash;
+        this.name = name;
+        this.path = path;
+
+
+    }
 }
 public class Pleasures : Count
 {
-    
+    public Pleasures(string name = "Empty", int cash = 0, string path = "pleasures.txt")
+    {
+        this.cash = cash;
+        this.name = name;
+        this.path = path;
+
+
+    }
 }
 public class Education : Count
 {
+    public Education(string name = "Empty", int cash = 0, string path = "education.txt")
+    {
+        this.cash = cash;
+        this.name = name;
+        this.path = path;
 
+
+    }
 }
 public class Freedom : Count
 {
+    public Freedom(string name = "Empty", int cash = 0, string path = "freedom.txt")
+    {
+        this.cash = cash;
+        this.name = name;
+        this.path = path;
 
+
+    }
 }
 public class Charity : Count
 {
-   
+    public Charity(string name = "Empty", int cash = 0, string path = "charity.txt")
+    {
+        this.cash = cash;
+        this.name = name;
+        this.path = path;
+
+
+    }
 }
 public class Earnings : Count
 {
--- Expenses/Program.cs
+++ Expenses/Program.cs
@@ -1,6 +1,22 @@
 ﻿using System.IO;
 
 
+
+
+static void Menu()
+{
+    Console.WriteLine("Select section");
+    Console.WriteLine("1: Life");
+    Console.WriteLine("2: Large expenses");
+    Console.WriteLine("3: Pleasures");
+    Console.WriteLine("4: Education");
+    Console.WriteLine("5: Freedoom");
+    Console.WriteLine("6: Charity");
+
+}
+
+int action;
+string s;
 Console.WriteLine("Welcome in Expenses");
 Console.WriteLine("Create by Patryk Pieczka");
 Earnings earnings = new Earnings();
@@ -9,19 +25,228 @@ Expenses expenses = new Expenses();
 expenses.Show();
 Savings savings = new Savings();
 savings.Show();
-Console.WriteLine("Select action");
-Console.WriteLine("1: Add a purchase");
-Console.WriteLine("2: Show a Purchase");
-Console.WriteLine("3: Determine your earnings");
-Console.WriteLine("4: Determine your expenses");
-Console.WriteLine("5: Determine your savings");
+
+
+do
+{
+    Console.WriteLine("Select action");
+    Console.WriteLine("1: Add a purchase");
+    Console.WriteLine("2: Show a Purchase");
+    Console.WriteLine("3: Determine your earnings");
+    Console.WriteLine("4: Determine your expenses");
+    Console.WriteLine("5: Determine your savings");
+    Console.WriteLine("0: Exit");
+
+    action = Int16.Parse(Console.ReadLine());
+    
+    switch (action)
+    {
+        case 1:
+            Menu();
+           
+            action = Int16.Parse(Console.ReadLine());
+
+            switch (action)
+            {
+                case 1:
+                    Life life = new Life();
+                    do
+                    {
+                        life.WriteText();
+                        Console.WriteLine("Do you want to add another expense (y/n)");
+                        s = Console.ReadLine();
+                    } while (s.Equals("y"));
+                    Console.WriteLine("Do you want to see your expenses (y/n)");
+                    s = Console.ReadLine();
+                    if (s.Equals("y"))
+                    {
+                        life.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                        Console.ReadKey();
+                    }
+
+
+                    break;
+
+                case 2:
+                    Large large = new Large();
+                    do
+                    {
+                        large.WriteText();
+                        Console.WriteLine("Do you want to add another expense (y/n)");
+                        s = Console.ReadLine();
+                    } while (s.Equals("y"));
+                    Console.WriteLine("Do you want to see your expenses (y/n)");
+                    s = Console.ReadLine();
+                    if (s.Equals("y"))
+                    {
+                        large.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                        Console.ReadKey();
+                    }
+
+                    break;
+
+                case 3:
+                    Pleasures pleasures = new Pleasures();
+                    do
+                    {
+                        pleasures.WriteText();
+                        Console.WriteLine("Do you want to add another expense (y/n)");
+                        s = Console.ReadLine();
+                    } while (s.Equals("y"));
+                    Console.WriteLine("Do you want to see your expenses (y/n)");
+                    s = Console.ReadLine();
+                    if (s.Equals("y"))
+                    {
+                        pleasures.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                        Console.ReadKey();
+                    }
+
+                    break;
+                case 4:
+                    Education education = new Education();
+                    do
+                    {
+                        education.WriteText();
+                        Console.WriteLine("Do you want to add another expense (y/n)");
+                        s = Console.ReadLine();
+                    } while (s.Equals("y"));
+                    Console.WriteLine("Do you want to see your expenses (y/n)");
+                    s = Console.ReadLine();
+                    if (s.Equals("y"))
+                    {
+                        education.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                        Console.ReadKey();
+                    }
+
+                    break;
+                case 5:
+                    Freedom freedom = new Freedom();
+                    do
+                    {
+                        freedom.WriteText();
+                        Console.WriteLine("Do you want to add another expense (y/n)");
+                        s = Console.ReadLine();
+                    } while (s.Equals("y"));
+                    Console.WriteLine("Do you want to see your expenses (y/n)");
+                    s = Console.ReadLine();
+                    if (s.Equals("y"))
+                    {
+                        freedom.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                        Console.ReadKey();
+                    }
+
+                    break;
+                case 6:
+                    Charity charity = new Charity();
+                    do
+                    {
+                        charity.WriteText();
+                        Console.WriteLine("Do you want to add another expense (y/n)");
+                        s = Console.ReadLine();
+                    } while (s.Equals("y"));
+                    Console.WriteLine("Do you want to see your expenses (y/n)");
+                    s = Console.ReadLine();
+                    if (s.Equals("y"))
+                    {
+                        charity.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                        Console.ReadKey();
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong section");
+                    break;
+            }
+            break;
+
+        case 2:
+            Menu();
+            action = Int16.Parse(Console.ReadLine());
+
+            switch (action)
+            {
+                case 1:
+                    Life life = new Life();
+                  
+                        life.ReadText();
+                        Console.WriteLine("Press the key to return to the menu");
+                    Console.ReadKey();
+                    break;
+
+                case 2:
+                    Large large = new Large();
+                   
+                    large.ReadText();
+                    Console.WriteLine("Press the key to return to the menu");
+                    Console.ReadKey();
+                    break;
+
+                case 3:
+                    Pleasures pleasures = new Pleasures();
+
+                    pleasures.ReadText();
+                    Console.WriteLine("Press the key to return to the menu");
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Education education = new Education();
+
+                    education.ReadText();
+                    Console.WriteLine("Press the key to return to the menu");
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    Freedom freedom = new Freedom();
+
+                    freedom.ReadText();
+                    Console.WriteLine("Press the key to return to the menu");
+                    Console.ReadKey();
+                    break;
+                case 6:
+                    Charity charity = new Charity();
+
+                    charity.ReadText();
+                    Console.WriteLine("Press the key to return to the menu");
+                    Console.ReadKey();
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong section");
+                    break;
+            }
+            break;
+
+        case 3:
+           
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 0:
+
+            break;
+
+        default:
+            Console.WriteLine("Wrong action");
+            break;
+    }
+} while (action != 0);
 
 
 
 
 
 
-Life life = new Life("meat" , 40);
 
 //life.WriteText();
 //life.ReadText();
@@ -30,5 +255,5 @@ Life life = new Life("meat" , 40);
 
 
 GC.Collect();
-//Console.ReadKey();
+
 
